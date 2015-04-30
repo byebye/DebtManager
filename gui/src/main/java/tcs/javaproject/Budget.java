73a1f043de3b4674196a43f45ec632738b5416eb
vsmasster <krasnal1995@gmail.com>
2015-04-30 19:48:01 +0200

    Add window with user's budgets (MainWindow)

---
+++ gui/src/main/java/tcs/javaproject/Budget.java
@@ -0,0 +1,29 @@
+package tcs.javaproject;
+
+/**
+ * Created by Vsmasster on 29.04.15.
+ */
+public class Budget {
+    private final String name;
+    private final String description;
+    private final int partNum;
+
+    public Budget(String _name,String _description,int _partNum){
+        this.name = _name;
+        this.description = _description;
+        this.partNum = _partNum;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public String getDescription(){
+        return description;
+    }
+
+    public int getPartNum(){
+        return partNum;
+    }
+
+}
---
+++ gui/src/main/java/tcs/javaproject/MainWindow.java
@@ -0,0 +1,26 @@
+package tcs.javaproject;
+
+/**
+ * Created by Vsmasster on 29.04.15.
+ */
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class MainWindow extends Application {
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("MainWindow.fxml"));
+        primaryStage.setTitle("DeptManager - My Budgets");
+        primaryStage.setScene(new Scene(root));
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
