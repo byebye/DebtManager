dca9d589af52fc38da94ee4832089cd619faad18
vsmasster <krasnal1995@gmail.com>
2015-05-07 13:19:49 +0200

    Updated BudgetWindow, now it is possible to view details on each payment. Summing all payments works.

--- server/src/main/java/tcs/javaproject/guitest/BudgetController.java
+++ server/src/main/java/tcs/javaproject/guitest/BudgetController.java
@@ -1,10 +1,25 @@
 package tcs.javaproject.guitest;
 
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableArray;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableRow;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.text.Text;
+import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.net.URL;
+import java.util.Observable;
 import java.util.ResourceBundle;
 
 public class BudgetController implements Initializable {
@@ -12,6 +27,16 @@ public class BudgetController implements Initializable {
    private Text txtBudgetName;
    @FXML
    private Text txtBudgetDescription;
+   @FXML
+   private Text txtSum;
+   @FXML
+   private Button btnAddPayment;
+   @FXML
+   private TableView<Payment> tabUnaccPayments;
+   @FXML
+   private TableColumn colWhat,colWho;
+   @FXML
+   private TableColumn colAmount;
 
    private Budget budget;
 
@@ -23,6 +48,47 @@ public class BudgetController implements Initializable {
 
    @Override
    public void initialize(URL location, ResourceBundle resources) {
+      //Buttons
+      btnAddPayment.setOnAction(event -> {
+         try {
+            AddPaymentWindow addPaymentWindow = new AddPaymentWindow(budget);
+            addPaymentWindow.show();
+         } catch (IOException e) {
+            e.printStackTrace();
+         }
+      });
 
+      //Table
+      colWhat.setCellValueFactory(new PropertyValueFactory<Payment, String>("what"));
+      colWho.setCellValueFactory(new PropertyValueFactory<Payment,String>("who"));
+      colAmount.setCellValueFactory(new PropertyValueFactory<Payment, Integer>("amount"));
+
+      final ObservableList<Payment> list = FXCollections.observableArrayList(
+              new Payment("John","Drinks",10),
+              new Payment("Marry","Food",20)
+      );
+      int sum = 0;
+      for(Payment p: list)
+         sum+=p.getAmount();
+
+      tabUnaccPayments.setItems(list);
+
+      tabUnaccPayments.setRowFactory(param -> {
+         TableRow<Payment> row = new TableRow<>();
+         row.setOnMouseClicked(mouseEvent -> {
+            if (mouseEvent.getClickCount() == 2 && !row.isEmpty()) {
+               Payment payment = row.getItem();
+               try {
+                  PaymentWindow paymentWindow = new PaymentWindow(payment);
+                  paymentWindow.show();
+               } catch (IOException e) {
+                  e.printStackTrace();
+               }
+            }
+         });
+         return row;
+      });
+
+      txtSum.setText("SUM: "+sum+"$");
    }
 }
