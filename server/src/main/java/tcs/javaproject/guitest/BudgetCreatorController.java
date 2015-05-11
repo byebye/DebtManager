2df68865529d45619311dc95d438b726b431e0b4
byebye <uaaabbjjkl@gmail.com>
2015-05-11 22:36:14 +0200

    Implement removing participants while creating new budget

--- server/src/main/java/tcs/javaproject/guitest/BudgetCreatorController.java
+++ server/src/main/java/tcs/javaproject/guitest/BudgetCreatorController.java
@@ -5,8 +5,11 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.geometry.Insets;
 import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
 import tcs.javaproject.database.DatabaseController;
 
@@ -20,13 +23,11 @@ public class BudgetCreatorController implements Initializable {
    @FXML
    private TextArea budgetDescription;
    @FXML
-   private Button btnCreateBudget, btnAddUser;
+   private Button btnCreateBudget, btnAddUser, btnCancel;
    @FXML
    private TableView<User> tabParticipants;
    @FXML
-   public TableColumn colParticipantName;
-   @FXML
-   public TableColumn colParticipantEmail;
+   public TableColumn colParticipantName, colParticipantEmail, colAction;
 
    private final DatabaseController dbController = LoginWindow.dbController;
    private int userId;
@@ -40,6 +41,7 @@ public class BudgetCreatorController implements Initializable {
    public void initialize(URL location, ResourceBundle resources) {
       colParticipantName.setCellValueFactory(new PropertyValueFactory<User, String>("name"));
       colParticipantEmail.setCellValueFactory(new PropertyValueFactory<User, String>("email"));
+      colAction.setCellFactory(param -> new RemoveParticipantCell());
       tabParticipants.setItems(participantsList);
 
       btnAddUser.setOnAction(event -> {
@@ -63,14 +65,45 @@ public class BudgetCreatorController implements Initializable {
          final User owner = dbController.getUserById(userId);
          participantsList.add(owner);
          Budget budget = new Budget(owner, budgetName.getText(), budgetDescription.getText(), participantsList);
-         if (dbController.createBudget(budget)) {
-            Stage stage = (Stage) btnCreateBudget.getScene().getWindow();
-            stage.close();
-         }
+         if (dbController.createBudget(budget))
+            close();
          else {
             // Error
          }
       });
+
+      btnCancel.setOnAction(event -> close());
+   }
+
+   private void close() {
+      Stage stage = (Stage) btnCancel.getScene().getWindow();
+      stage.close();
+   }
+
+   private class RemoveParticipantCell extends TableCell<User, Boolean> {
+      final Button btnRemove = new Button();
+
+      public RemoveParticipantCell() {
+         setPadding(new Insets(0, 0, 0, 0));
+         btnRemove.setPrefSize(25, 25);
+         Image removeImage = new Image(getClass().getResourceAsStream("/graphics/remove_button.png"));
+         ImageView removeImageView = new ImageView(removeImage);
+         removeImageView.setPreserveRatio(true);
+         removeImageView.setFitHeight(12);
+         btnRemove.setPadding(new Insets(5, 5, 5, 5));
+         btnRemove.setGraphic(removeImageView);
+         btnRemove.setOnAction(event -> {
+            User participant = (User) RemoveParticipantCell.this.getTableRow().getItem();
+            participantsList.remove(participant);
+         });
+      }
+
+      @Override
+      protected void updateItem(Boolean item, boolean empty) {
+         super.updateItem(item, empty);
+         if (!empty)
+            setGraphic(btnRemove);
+      }
    }
 }
 
