ee194b8ea54c7d14cd0e668a800c0f5bf946b6e4
Michał Glapa <michal.glapa@wp.pl>
2015-05-12 13:07:48 +0200

    makefiles adjusted

--- RMIServer/src/main/java/startServer.sh
+++ RMIServer/src/main/java/startServer.sh
@@ -1,11 +1,13 @@
-URL=http://michalglapa.student.tcs.uj.edu.pl
+HOST=michalglapa.student.tcs.uj.edu.pl
+URL=http://$HOST
+JAVA=java
 killall rmiregistry
 rmiregistry -J-Djava.rmi.server.codebase=$URL/classes/ &
 
 echo "Registry started"
-java -Djava.security.policy=AllPermissions.policy \
+$JAVA -Djava.security.policy=AllPermissions.policy \
          -Djava.rmi.server.codebase=$URL/classes/ \
-         -Djava.rmi.server.hostname=$URL \
+         -Djava.rmi.server.hostname=$HOST \
          server.RMIServer
 
 
