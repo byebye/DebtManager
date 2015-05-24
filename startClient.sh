7a13f909b3e9f3837282e6ed0c31de8b001cb9b2
Michał Glapa <michal.glapa@gmail.com>
2015-05-24 21:44:20 +0200

    startClient fixed

--- startClient.sh
+++ startClient.sh
@@ -1,12 +1,11 @@
 HOST=localhost
 URL=http://$HOST
-JDK=/usr/lib/jvm/java-8-jdk/bin
+JDK=/usr/lib/jvm/java-8-oracle/bin
 JAVA=$JDK/java
 VERSION=0.0.1
 CODEBASE=$PWD/common/target/common-$VERSION.jar
 
-$JAVA -cp client/target/client-$VERSION.jar:common/target/common-$VERSION.jar \
-      -Djava.security.policy=Allpermissions.policy \
+$JAVA -Djava.security.policy=Allpermissions.policy \
       -Djava.rmi.server.codebase=file:///$CODEBASE \
       -Djava.rmi.server.hostname=$HOST \
-      client.windows.LoginWindow
+      -jar ./client_release/target/client-all-jar.jar 
