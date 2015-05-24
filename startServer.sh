e3e051b873849c98c97ea20c3923209b67f30a70
Michał Glapa <michal.glapa@gmail.com>
2015-05-24 21:45:03 +0200

    startServer fixed

--- startServer.sh
+++ startServer.sh
@@ -3,9 +3,11 @@ URL=http://$HOST
 JDK=~/jdk1.8.0_45/bin
 JAVA=$JDK/java
 RMIREGISTRY=$JDK/rmiregistry
+JAR=./server_release/target/server-all-jar.jar
 
 
 cp common/target/common*.jar ~/public_html/classes/common.jar
+
 killall rmiregistry
 
 sleep 1
@@ -15,6 +17,6 @@ echo "Registry started"
 $JAVA   -Djava.security.policy=Allpermissions.policy \
         -Djava.rmi.server.codebase=$URL/classes/common.jar \
         -Djava.rmi.server.hostname=$HOST \
-        -jar ./server_release/target/server-all-jar.jar  -u debtmanager -p debtmanager -d localhost/debtmanager &
+        -jar $JAR  -u debtmanager -p debtmanager -d localhost/debtmanager &
 
 
