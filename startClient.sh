89df5a565b06d52e507320139130521bd4943d3f
Michał Glapa <michal.glapa@gmail.com>
2015-05-30 14:54:24 +0200

    added sendgrid and a class testing it

--- startClient.sh
+++ startClient.sh
@@ -1,11 +1,11 @@
-HOST=localhost
+HOST=managedebtsfor.me
 URL=http://$HOST
-JDK=/usr/lib/jvm/java-8-oracle/bin
+JDK=/usr/bin
 JAVA=$JDK/java
 VERSION=0.0.1
 CODEBASE=$PWD/common/target/common-$VERSION.jar
 
 $JAVA -Djava.security.policy=Allpermissions.policy \
-      -Djava.rmi.server.codebase=file:///$CODEBASE \
+      -Djava.rmi.server.codebase=$URL/classes/common.jar \
       -Djava.rmi.server.hostname=$HOST \
-      -jar ./client_release/target/client-all-jar.jar --host="localhost"
+      -jar ./client_release/target/client-all-jar.jar --host=$HOST
