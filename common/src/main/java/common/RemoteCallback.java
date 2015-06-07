a9883760a155a35978ebe05dcca13b750af7f3fb
Michał Glapa <michal.glapa@gmail.com>
2015-06-07 10:01:04 +0200

    added some callback testing, sorry for placing it in master amend : quickfix

---
+++ common/src/main/java/common/RemoteCallback.java
@@ -0,0 +1,12 @@
+package common;
+
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+import java.util.concurrent.Callable;
+
+/**
+ * Created by glapul on 07.06.15.
+ */
+public interface RemoteCallback extends Remote {
+   void call() throws RemoteException;
+}
---
+++ server/src/main/java/server/UserSpecificDBHandler.java
@@ -0,0 +1,9 @@
+package server;
+
+import common.DBHandler;
+
+/**
+ * Created by glapul on 05.06.15.
+ */
+public class UserSpecificDBHandler /*implements DBHandler*/ {
+}
