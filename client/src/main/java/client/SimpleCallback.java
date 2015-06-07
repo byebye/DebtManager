4878989fa72d0403a4afadf6bc2d3ce7f3302559
Michał Glapa <michal.glapa@gmail.com>
2015-06-07 11:17:03 +0200

    still not ok

--- client/src/main/java/client/SimpleCallback.java
+++ client/src/main/java/client/SimpleCallback.java
@@ -8,13 +8,14 @@ import java.rmi.server.UnicastRemoteObject;
 /**
  * Created by glapul on 07.06.15.
  */
-public class SimpleCallback extends UnicastRemoteObject implements RemoteCallback {
-
-   protected SimpleCallback() throws RemoteException {
-   }
-
+public class SimpleCallback  implements RemoteCallback {
    @Override
    public void call() throws RemoteException {
       System.out.println("Callback called");
+   }
+
+   @Override
+   protected void finalize() {
+      System.out.println("Callback gc'd");
    }
 }
