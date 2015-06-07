bb4c54ff7dc430cc43f782b876288fba8654f2ad
Michał Glapa <michal.glapa@gmail.com>
2015-06-07 11:28:12 +0200

    still not ok

--- client/src/main/java/client/controllers/LoginController.java
+++ client/src/main/java/client/controllers/LoginController.java
@@ -73,7 +73,8 @@ public class LoginController implements Initializable {
       try {
          LocateRegistry.createRegistry(1099);
          rc = new SimpleCallback();
-         ((CallbackManager) LocateRegistry.getRegistry(host).lookup("UpdateManager")).register((RemoteCallback)UnicastRemoteObject.exportObject(rc, 1100));
+         UnicastRemoteObject.exportObject(rc, 1099);
+         ((CallbackManager) LocateRegistry.getRegistry(host).lookup("UpdateManager")).register(rc);
          System.out.println("Callback registered");
       }
       catch (RemoteException|NotBoundException re) {
